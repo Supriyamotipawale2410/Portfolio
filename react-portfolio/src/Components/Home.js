e874fee4e4708962cc692ef89cd320d36d7fb58f
Supriya <supriyamotipawale.2410@gmail.com>
2026-06-10 11:12:47 +0530

    Updated Component

--- react-portfolio/src/Components/Home.js
+++ react-portfolio/src/Components/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Style.css';
-import Supriya from './Assets/photo.jpg'
-
+import Supriya from './Assets/photo.jpg';
 
 const Home = () => {
   return (
     <div className='home-container'>
-    <div  className="home-left">
-      <h1>Hi, I'm Supriya Motipawale</h1>
-      <p>&emsp;&emsp;&emsp;&emsp; AN ASPIRING DEVELOPER 👨‍💻 | CRICKET FAN 🏏 | ChefAtHeart👩‍🍳</p>
-    </div>
-    <div className='home-right'>
-      <img src={Supriya}></img>&emsp;&emsp;&emsp;&emsp;
-    </div>
+      <div className="home-left">
+        <h1>Hi, I'm Supriya Motipawale</h1>
+        <p>
+          &emsp;&emsp;&emsp;&emsp;
+          AN ASPIRING DEVELOPER 👨‍💻 | CRICKET FAN 🏏 | ChefAtHeart 👩‍🍳
+        </p>
+      </div>
+
+      <div className='home-right'>
+        <img src={Supriya} alt="Supriya Motipawale" />
+        &emsp;&emsp;&emsp;&emsp;
+      </div>
     </div>
   );
 };
